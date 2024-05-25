ae914c2ad183805f18491b83e01b22516fb6e8e0
moktanBishal <20saallama@gmail.com>
2024-05-25 16:40:50 +0545

    bugfix server code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const moment = require('moment');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,23 +9,44 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', ws => {
+const clients = new Map();
+
+wss.on('connection', (ws) => {
   console.log('New client connected');
+  clients.set(ws, {});
 
-  ws.on('message', message => {
-    console.log(`Received: ${message}`);
-    const parsedMessage = JSON.parse(message);
+  ws.send(JSON.stringify({ type: 'systemMessage', message: 'Welcome to the WebSocket chat!' }));
 
-    // Broadcast the received message to all connected clients
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'chatMessage', message: parsedMessage.message }));
+  ws.on('message', (message) => {
+    try {
+      const data = JSON.parse(message);
+      if (data.type === 'setNickname') {
+        clients.get(ws).nickname = data.nickname;
+        ws.send(JSON.stringify({ type: 'systemMessage', message: `Your nickname has been set to ${data.nickname}` }));
+      } else if (data.type === 'chatMessage') {
+        const nickname = clients.get(ws).nickname || 'Anonymous';
+        const timestamp = moment().format('HH:mm:ss');
+        const chatMessage = {
+          type: 'chatMessage',
+          nickname: nickname,
+          message: data.message,
+          timestamp: timestamp
+        };
+        // Broadcast message to all clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(chatMessage));
+          }
+        });
       }
-    });
+    } catch (error) {
+      console.error('Error parsing message', error);
+    }
   });
 
   ws.on('close', () => {
     console.log('Client disconnected');
+    clients.delete(ws);
   });
 });
 
