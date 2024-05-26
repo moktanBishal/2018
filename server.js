64c5e78c8a9181fdadf40386961d07d1df6cd29c
moktanBishal <20saallama@gmail.com>
2024-05-26 16:43:38 +0545

    bugfix server file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const moment = require('moment'); // Ensure moment is imported
+const moment = require('moment');
+// const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,11 +12,25 @@ const wss = new WebSocket.Server({ server });
 
 const clients = new Map();
 
+// Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, '../frontend')));
+
+function broadcastUserCount() {
+  const userCount = clients.size;
+  const message = JSON.stringify({ type: 'userCount', count: userCount });
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
 wss.on('connection', (ws) => {
   console.log('New client connected');
   clients.set(ws, {});
 
   ws.send(JSON.stringify({ type: 'systemMessage', message: 'Welcome to the WebSocket chat!' }));
+  broadcastUserCount();
 
   ws.on('message', (message) => {
     try {
@@ -47,11 +62,13 @@ wss.on('connection', (ws) => {
   ws.on('close', () => {
     console.log('Client disconnected');
     clients.delete(ws);
+    broadcastUserCount();
   });
 });
 
 app.get('/', (req, res) => {
-  res.send('WebSocket server is running');
+  res.send('Websocket is running properly.')
+  // res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 server.listen(port, () => {
