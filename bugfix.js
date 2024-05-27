918a688d31a9a4be30f72093b975ab8e616d592b
moktanBishal <20saallama@gmail.com>
2024-05-27 13:22:06 +0545

    bugfix bugfix

--- bugfix.js
+++ bugfix.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     
   async function connectToChatApp() {
     // Establish a WebSocket connection to the server
-    const ws = new WebSocket('ws://192.168.1.64:3000');
+    const ws = new WebSocket('wss://websocket-rd9d.onrender.com');
 
     // Function to send a JSON message to the server
     function sendJsonMessage(type, data) {
