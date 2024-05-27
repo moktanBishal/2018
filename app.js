3000d7b42f0e0897637bee79999107b13a72f620
moktanBishal <20saallama@gmail.com>
2024-05-27 13:15:08 +0545

    bugfix server code

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const path = require('path');
 const send = require('send');
 const { handleWebSocketConnection } = require('./websocketHandler');
 
-const server = http.createServer((req, res) => {
-    // let filePath = req.url === '/' ? '/chat.html' : req.url;
-    // filePath = path.join(__dirname, '../frontend', filePath);
-    // send(req, filePath)
-    //     .on('error', (err) => {
-    //         console.error(err);
-    //         res.writeHead(404, { 'Content-Type': 'text/plain' });
-    //         res.end('404 Not Found');
-    //     })
-    //     .pipe(res);
-    res.send('Server is running');
-});
+const server = http.createServer();
+// const server = http.createServer((req, res) => {
+//     // let filePath = req.url === '/' ? '/chat.html' : req.url;
+//     // filePath = path.join(__dirname, '../frontend', filePath);
+//     // send(req, filePath)
+//     //     .on('error', (err) => {
+//     //         console.error(err);
+//     //         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//     //         res.end('404 Not Found');
+//     //     })
+//     //     .pipe(res);
+// });
 
 // Initialize WebSocket connection handler
 handleWebSocketConnection(server);
